923b30f8b0b887fb29d5d92d63653faca979e3d0
sparvanov <s.iv.parvanov@gmail.com>
2018-11-20 14:49:12 +0000

    cleanup

---
+++ TodoList.React/ClientApp/components/About.tsx
@@ -0,0 +1,11 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+
+export class About extends React.Component<RouteComponentProps<{}>, {}> {
+    public render() {
+        return <div>
+            <h1>About</h1>
+            <p>Use this area to provide additional information.</p>
+        </div>;
+    }
+}
