188585e38816d5573fc9c8c9a1b6a4b6b741765d
sparvanov <s.iv.parvanov@gmail.com>
2018-11-20 16:08:40 +0000

    recommited

---
+++ TodoList.AspNetCoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,118 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DataAccess.Entities;
+using TodoList.Models;
+using Services;
+using System.Linq;
+using System;
+using System.Diagnostics;
+
+namespace TodoList.AspNetCoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITodoViewModel _model;
+        private readonly ITodoService _service;
+
+        public HomeController( ITodoService service, ITodoViewModel model)
+        {
+            _service = service;
+            _model = model;
+        }
+
+        public async Task<IActionResult> List(int? id = 1)
+        {
+            ViewBag.Pages = await _service.GetPageNumber();
+            ViewBag.CurrentPage = id;
+            _model.List = await _service.GetData(id.GetValueOrDefault());
+            if (TempData["errorMsg"] != null)
+            {
+                ViewData["errorMsg"] = TempData["errorMsg"];
+            }
+            return View(_model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TodoId,Description,IsDone")] Todo todo)
+        {
+            if (ModelState.IsValid)
+            {
+                await _service.Create(todo);
+                return RedirectToAction(nameof(List));
+            }
+            var errors =ModelState.Values.SelectMany(x => x.Errors);
+            string err = String.Empty;
+            foreach(var e in errors)
+            {
+                err += e.ErrorMessage;
+            }
+            TempData["errorMsg"] = err;
+            return RedirectToAction(nameof(List));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit( string submitButton, [Bind("TodoId,Description,IsDone")] Todo todo)
+        {
+            switch(submitButton)
+            {
+                case "Save":
+                    {
+                        return await Update(todo);
+                    }
+                case "Delete":
+                    {
+                        return await Delete(todo.TodoId);
+                    }
+                default:
+                    {
+                        return RedirectToAction(nameof(List));
+                    }
+            }
+        }
+
+        private async Task<IActionResult> Update(Todo todo)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _service.Update(todo);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TodoExists(todo.TodoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return Redirect(HttpContext.Request.Headers["Referer"]);
+            }
+            return Redirect(HttpContext.Request.Headers["Referer"]);
+        }
+
+        private async Task<IActionResult> Delete(int id)
+        {
+            await _service.Delete(id);
+            return Redirect(HttpContext.Request.Headers["Referer"]);
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private bool TodoExists(int id)
+        {
+            return _service.GetById(id) != null;
+        }
+    }
+}
---
+++ TodoList.AspNetCoreMVC/Models/TodoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Models
+{
+    public class TodoViewModel : ITodoViewModel
+    {
+        public int Pages { get; set; }
+        public int CurrentPage { get; set; }
+        public Todo Entity { get; set; }
+        public IEnumerable<Todo> List { get; set; }
+    }
+}
