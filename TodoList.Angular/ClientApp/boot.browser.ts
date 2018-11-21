761015de366eb1949533215b632998e4c212bf8e
sparvanov <s.iv.parvanov@gmail.com>
2018-11-21 11:47:17 +0000

    updated bootstrap

--- TodoList.Angular/ClientApp/boot.browser.ts
+++ TodoList.Angular/ClientApp/boot.browser.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import 'zone.js';
+import 'popper.js';
 import 'bootstrap';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
